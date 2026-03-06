05fafb508deacbbb52211d336ee6844b5f3d89bd
Abhilasha Lahare <abhilashalahare6@gmail.com>
2026-03-06 13:04:21 +0530

    url updated

--- frontend/src/store/useAuthstore.js
+++ frontend/src/store/useAuthstore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 
